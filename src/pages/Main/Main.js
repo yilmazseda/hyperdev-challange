70746ff9ca9cf697221c1f4908ca49fbb46d5d53
sedayilmaz <ylmzseda60@gmail.com>
2022-06-09 19:21:07 +0300

    created Navbar

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+const Main = () => {
+  return (
+    <div className="flex h-screen ">
+      <div className="m-auto w-full h-screen rounded">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontNavbar: "'Cormorant Garamond',sans-serif",
+      },
+      colors: {
+        "navbar-color": "#b08ead",
+        "border-top-color": "#d6bbd3",
+      },
+    },
+  },
+
+  plugins: [],
+};
